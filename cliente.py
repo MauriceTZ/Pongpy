cd576826884cb7e778ece1346ac2b772b11275ef
Maurice <106934566+MauriceTZ@users.noreply.github.com>
2024-08-21 09:26:50 -0400

    se comunican XD

--- cliente.py
+++ cliente.py
@@ -1,25 +1,29 @@
 import pygame as pg
 import socket
 
-SIZE = (1000, 1000)
+
+SIZE = 1000, 1000
 CLOCK = pg.time.Clock()
 FPS = 60
+
 
 SERVER_ADDRESS = socket.gethostname(), 6969
 clientsocket = socket.socket()
 clientsocket.connect(SERVER_ADDRESS)
-print("HOLA", clientsocket)
+mensaje = "PONG"
+clientsocket.send(mensaje.encode())
+print("enviado:", mensaje)
 
-pg.init()
-pg.display.set_caption("Pongpy")
-pg.display.set_mode(SIZE)
+# pg.init()
+# pg.display.set_caption("Pongpy")
+# pg.display.set_mode(SIZE)
 
-while True:
-    for evento in pg.event.get():
-        match evento.type:
-            case pg.QUIT:
-                pg.quit()
-                exit()
+# while True:
+#     for evento in pg.event.get():
+#         match evento.type:
+#             case pg.QUIT:
+#                 pg.quit()
+#                 exit()
 
-    pg.display.update()
-    CLOCK.tick(FPS)
+#     pg.display.update()
+#     CLOCK.tick(FPS)
--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 import socket
 
+
 SERVER_ADDRESS = socket.gethostname(), 6969
 serversocket = socket.socket()
 serversocket.bind(SERVER_ADDRESS)
 serversocket.listen()
-
+print("esperando conexiones...")
 
 while True:
     (clientsocket, address) = serversocket.accept()
-    print(clientsocket)
+    print("recibido:", clientsocket.recv(100).decode())
     clientsocket.close()
+
