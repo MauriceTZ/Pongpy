7b627545a4150a9695ddbf5f829cb558b10b57fb
Maurice <106934566+MauriceTZ@users.noreply.github.com>
2024-08-23 10:04:14 -0400

    funciona

--- cliente.py
+++ cliente.py
@@ -1,29 +1,97 @@
 import pygame as pg
 import socket
+import threading
+import json
+import sys
 
 
-SIZE = 1000, 1000
-CLOCK = pg.time.Clock()
+SIZE_WINDOW = 900, 900
+SERVER_ADDRESS = socket.gethostname(), 1234
+PACKET_SIZE = 256
 FPS = 60
+CLOCK = pg.time.Clock()
+RECT_SIZE = 40, 200
+RECT_COLOR = 255, 255, 255
+OPONENT_RECT_COLOR = 255, 0, 0
+BACKGROUND_COLOR = 0, 0, 0
+BALL_RADIUS = 50
+BALL_COLOR = 0, 0, 255
+LINE_COLOR = 0, 255, 0
+TEXT_COLOR = 255, 255, 255
 
-
-SERVER_ADDRESS = socket.gethostname(), 6969
 clientsocket = socket.socket()
 clientsocket.connect(SERVER_ADDRESS)
-mensaje = "PONG"
-clientsocket.send(mensaje.encode())
-print("enviado:", mensaje)
 
-# pg.init()
-# pg.display.set_caption("Pongpy")
-# pg.display.set_mode(SIZE)
+# oponent_y_pos = 0
+ball_pos = pg.Vector2(SIZE_WINDOW[0]/2, SIZE_WINDOW[1]/2)
+rect = pg.Rect(0, 0, *RECT_SIZE)
+points = 0
+oponent_rect = pg.Rect(0, 0, *RECT_SIZE)
+oponent_points = 0
+end_thread = False
 
-# while True:
-#     for evento in pg.event.get():
-#         match evento.type:
-#             case pg.QUIT:
-#                 pg.quit()
-#                 exit()
+print("conectado al servidor, esperando oponente...")
 
-#     pg.display.update()
-#     CLOCK.tick(FPS)
+pg.init()
+pg.display.set_caption("Pongpy")
+window = pg.display.set_mode(SIZE_WINDOW)
+font = pg.font.SysFont("timesnewroman", 30)
+
+
+while not (init_data := json.loads(clientsocket.recv(PACKET_SIZE))).get("status") == "OK":
+    pass
+rect = pg.Rect(init_data.get("rect"))
+
+print("oponente conectado, partida iniciada")
+
+
+def thread_recieve_data():
+    global oponent_rect, ball_pos, BALL_RADIUS, points, oponent_points
+    while not end_thread:
+        try:
+            data = json.loads(clientsocket.recv(PACKET_SIZE))
+            oponent_rect = pg.Rect(*data.get("rect", oponent_rect))
+            ball_pos = pg.Vector2(data.get("ball_pos", ball_pos))
+            BALL_RADIUS = float(data.get("ball_radius", BALL_RADIUS))
+            points = int(data.get("your_points", points))
+            oponent_points = int(data.get("oponent_points", oponent_points))
+        except json.JSONDecodeError:
+            pass
+
+
+threading.Thread(target=thread_recieve_data).start()
+
+try:
+    while True:
+        window.fill(BACKGROUND_COLOR)
+        for evento in pg.event.get():
+            match evento.type:
+                case pg.QUIT:
+                    end_thread = True
+                    pg.quit()
+                    sys.exit()
+
+        mouse_pos = pg.mouse.get_pos()
+        rect.centery = mouse_pos[1]
+        pg.draw.rect(window, RECT_COLOR, rect)
+        pg.draw.rect(window, OPONENT_RECT_COLOR, oponent_rect)
+        pg.draw.line(window, LINE_COLOR, window.get_rect().midtop,
+                     window.get_rect().midbottom)
+        pg.draw.circle(window, BALL_COLOR, ball_pos, BALL_RADIUS)
+        po = font.render(f"Puntos oponente: {oponent_points}", False, TEXT_COLOR)
+        window.blit(
+            po,
+            ((oponent_rect.centerx + 50)* 0.7, 10),
+        )
+        p = font.render(f"Puntos: {points}", False, TEXT_COLOR)
+        window.blit(
+            p,
+            ((rect.centerx + 50) * 0.7, 10),
+        )
+        clientsocket.send(json.dumps({"rect": tuple(rect)}).encode())
+        pg.display.update()
+        CLOCK.tick(FPS)
+except Exception as e:
+    print(e)
+finally:
+    end_thread = True
--- server.py
+++ server.py
@@ -1,14 +1,133 @@
 import socket
+import pygame as pg
+import threading
+import json
+import time
+import random
+import math
 
+SIZE_WINDOW = 900, 900
+SERVER_ADDRESS = socket.gethostname(), 1234
+PACKET_SIZE = 256
+FPS = 60
+CLOCK = pg.time.Clock()
+RECT_SIZE = 40, 200
+BALL_RADIUS = 50
+BALL_INITIAL_VELOCITY_MAGNITUDE = 300
+WINDOW_RECT = pg.Rect(0, 0, *SIZE_WINDOW)
 
-SERVER_ADDRESS = socket.gethostname(), 6969
 serversocket = socket.socket()
 serversocket.bind(SERVER_ADDRESS)
 serversocket.listen()
-print("esperando conexiones...")
 
-while True:
-    (clientsocket, address) = serversocket.accept()
-    print("recibido:", clientsocket.recv(100).decode())
-    clientsocket.close()
+end_thread = False
 
+print("esperando jugadores...")
+
+
+class Cliente:
+    def __init__(self, socket: socket.socket) -> None:
+        self.socket = socket
+        self.rect = pg.Rect(0, 0, *RECT_SIZE)
+        self.points = 0
+
+
+class Ball:
+    def __init__(self) -> None:
+        self.position = pg.Vector2(SIZE_WINDOW[0]/2, SIZE_WINDOW[1]/2)
+        angle = math.radians(random.randint(25, 70))
+        self.velocity = pg.Vector2(
+            math.sin(angle), math.cos(angle)) * BALL_INITIAL_VELOCITY_MAGNITUDE
+        self.rect = pg.Rect(0, 0, BALL_RADIUS*2, BALL_RADIUS*2)
+        self.rect.center = self.position
+
+
+clientes: list[Cliente] = []
+ball = Ball()
+
+# Esperar a los clientes
+for i in range(2):
+    socketclient, _ = serversocket.accept()
+    clientes.append(Cliente(socketclient))
+    print(f"jugador {i} conectado")
+
+clientes[1].rect.right = WINDOW_RECT.right
+
+
+def thread_client_recieve(cliente: Cliente):
+    while not end_thread:
+        try:
+            data = json.loads(cliente.socket.recv(PACKET_SIZE))
+            cliente.rect.top = pg.Rect(data.get("rect", cliente.rect)).top
+        except json.JSONDecodeError:
+            pass
+
+
+def cambio_random():
+    global BALL_RADIUS
+    if random.randint(0, 30) == 15:
+        ball.velocity *= 0.80
+        BALL_RADIUS *= 0.70
+
+
+print("partida iniciada")
+
+try:
+    clientes[0].socket.send(json.dumps(
+        {"status": "OK", "rect": tuple(clientes[0].rect)}).encode())
+    threading.Thread(target=thread_client_recieve, args=(clientes[0],)).start()
+
+    clientes[1].socket.send(json.dumps(
+        {"status": "OK", "rect": tuple(clientes[1].rect)}).encode())
+    threading.Thread(target=thread_client_recieve, args=(clientes[1],)).start()
+
+    time.sleep(1)
+    # es para que la bola no salga a la chucha al inicio (el get_time() toma el tiempo entre
+    # las dos ultimas llamadas de tick(FPS))
+    CLOCK.tick(FPS)
+    CLOCK.tick(FPS)
+    while True:
+        ball.rect.center = ball.position
+        ball.rect.size = 2 * BALL_RADIUS, 2 * BALL_RADIUS
+
+        if ball.rect.left <= WINDOW_RECT.left or ball.rect.right >= WINDOW_RECT.right:
+            ball.velocity.x *= -1
+            if ball.rect.left <= WINDOW_RECT.left:
+                clientes[1].points += 1
+            else:
+                clientes[0].points += 1
+
+        if ball.rect.top <= WINDOW_RECT.top or ball.rect.bottom >= WINDOW_RECT.bottom:
+            ball.velocity.y *= -1
+
+        ball.rect = ball.rect.clamp(WINDOW_RECT)
+
+        if ball.rect.colliderect(clientes[0].rect):
+            ball.velocity.x = abs(ball.velocity.x)
+            ball.velocity.x += 15
+            ball.velocity.y += random.choice((-100, 100))
+            cambio_random()
+
+        if ball.rect.colliderect(clientes[1].rect):
+            ball.velocity.x = -abs(ball.velocity.x)
+            ball.velocity.x -= 15
+            ball.velocity.y += random.choice((-100, 100))
+            cambio_random()
+
+        ball.position += ball.velocity * CLOCK.get_time() * 0.001
+
+        clientes[0].socket.send(json.dumps({"rect": tuple(clientes[1].rect),
+                                            "ball_pos": tuple(ball.position),
+                                            "ball_radius": BALL_RADIUS,
+                                            "your_points": clientes[0].points,
+                                            "oponent_points": clientes[1].points}).encode())
+        clientes[1].socket.send(json.dumps({"rect": tuple(clientes[0].rect),
+                                            "ball_pos": tuple(ball.position),
+                                            "ball_radius": BALL_RADIUS,
+                                            "your_points": clientes[1].points,
+                                            "oponent_points": clientes[0].points}).encode())
+        CLOCK.tick(FPS)
+except Exception as e:
+    print(e)
+finally:
+    end_thread = True
